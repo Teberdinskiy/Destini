a1f469d19461ae36ed4921b3a88f93342deb4376
Alikhan Batchaev <loko-han@mail.ru>
2020-01-25 17:48:56 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by 🧔🏻Alikhan Batchaev on 25.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
